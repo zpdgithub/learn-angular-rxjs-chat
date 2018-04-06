816087fe688aeafe906ce5f687efc0bb369c0331
peter <peter@soez.io>
2018-04-06 15:49:03 +0800

    添加Thread类

--- src/app/ts/models.ts
+++ src/app/ts/models.ts
@@ -10,3 +10,18 @@ export class User {
     }
 }
 
+export class Thread {
+    id: string;
+    lastMessage: Message;
+    name: string;
+    avatarSrc: string;
+
+    constructor(id?: string,
+        name?: string,
+        avatarSrc?: string) {
+        this.id = id || uuid();
+        this.name = name;
+        this.avatarSrc = avatarSrc;
+    }
+}
+
