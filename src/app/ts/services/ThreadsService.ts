f5f1ab9feb5f8e0110fbdaee3243d21797cbe54d
peter <peter@soez.io>
2018-04-07 20:13:43 +0800

    定义ThreadsService类

---
+++ src/app/ts/services/ThreadsService.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Subject, BehaviorSubject, Observable } from 'rxjs';
+import { Thread, Message } from '../models';
+import { MessagesService } from './MessagesService';
+import * as _ from 'underscore';
+
+@Injectable()
+export class ThreadsService {
+}
